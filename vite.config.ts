b8d4d8e8a86025101a9704a34dc589b530ee29d4
Sent1nelX <127220411+Sent1nelX@users.noreply.github.com>
2024-12-17 22:55:43 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,14 @@ export default defineConfig({
   server: {
     host: true,
     proxy: {
+      // Прокси для запросов на API
       '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '/api')
       },
+      // Прокси для запросов на медиафайлы
       '/media': {
         target: 'http://localhost:3000',
         changeOrigin: true,
